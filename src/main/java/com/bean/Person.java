a57d798ca33d2a17de31dc7d672df5212ee2bea2
sun <837544713@qq.com>
2019-11-07 15:40:54 +0800

    add autowired mysql

--- src/main/java/com/bean/Person.java
+++ src/main/java/com/bean/Person.java
@@ -1,5 +1,8 @@
 package com.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
     public String name;
     public String age;
--- src/main/java/com/main/Run1107.java
+++ src/main/java/com/main/Run1107.java
@@ -1,11 +1,21 @@
 package com.main;
 
+import com.bean.Coach;
+import com.config.MysqlConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Run1107 {
     public static void main(String[] args){
         //ApplicationContext context = new ClassPathXmlApplicationContext("classpath:context.xml");
-          //ApplicationContext context=new A
+        //ApplicationContext context=new AnnotationConfigApplicationContext(MysqlConfig.class);
+        //((AnnotationConfigApplicationContext) context).register();
+        //Coach coach=context.getBean(Coach.class);
+        AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext();
+        applicationContext.register(MysqlConfig.class);
+        applicationContext.refresh();
+        Coach coach=applicationContext.getBean(Coach.class);
+        System.out.println(coach.name);
     }
 }
